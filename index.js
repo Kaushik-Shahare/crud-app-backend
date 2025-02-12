de2d96f12f33218476462ded8fbf19967b342d01
Kaushik-Shahare <kaushikshahare0@gmail.com>
2025-02-12 11:28:10 +0530

    Added API not found and Server runing status page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-import bodyParser from "body-parser"
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import cors from "cors";
 import route from "./routes/userRoute.js";
@@ -10,19 +10,25 @@ app.use(bodyParser.json());
 app.use(cors());
 dotenv.config();
 
-
 const PORT = process.env.PORT || 7000;
 const URL = process.env.MONGO_URL;
 
-
-mongoose.connect(URL, { useNewURLParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("DB connected successfully");
-        app.listen(PORT, () => {
-        console.log(`Server is running on port: ${PORT}`);
-    })
-
-}).catch(error => console.log(error));
-
+mongoose
+  .connect(URL, { useNewURLParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("DB connected successfully");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(error));
 
 app.use("/api", route);
+
+app.get("/", (req, res) => {
+  res.send("Server is running.");
+});
+
+app.get("*", (req, res) => {
+  res.send("API not found");
+});
